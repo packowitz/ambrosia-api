bbc2ab7d5f76297c9bc503344493a16966fcd0a9
packowitz <philipp.schumacher.external@zalando.de>
2019-11-14 10:35:11 +0100

    props

--- src/main/kotlin/io/pacworx/ambrosia/admin/controller/AdminPropertyController.kt
+++ src/main/kotlin/io/pacworx/ambrosia/admin/controller/AdminPropertyController.kt
@@ -4,12 +4,20 @@ import io.pacworx.ambrosia.io.pacworx.ambrosia.enums.PropertyType
 import io.pacworx.ambrosia.io.pacworx.ambrosia.models.DynamicProperty
 import io.pacworx.ambrosia.io.pacworx.ambrosia.models.DynamicPropertyRepository
 import io.pacworx.ambrosia.io.pacworx.ambrosia.services.PropertyService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin(maxAge = 3600)
 @RequestMapping("admin/properties")
 class AdminPropertyController(private val propertyService: PropertyService) {
+
+    @Value("\${ambrosia.jwt-secret}")
+    private lateinit var jwtSecret: String
+    @Value("\${ambrosia.pw-salt-one}")
+    private lateinit var pwSalt1: String
+    @Value("\${ambrosia.pw-salt-two}")
+    private lateinit var pwSalt2: String
 
     @PostMapping("type/{type}")
     fun saveProperties(@PathVariable("type") type: PropertyType, @RequestBody properties: List<DynamicProperty>): List<DynamicProperty> {
@@ -21,4 +29,11 @@ class AdminPropertyController(private val propertyService: PropertyService) {
         propertyService.deleteProperty(type, id)
         return propertyService.getAllProperties(type)
     }
+
+    @GetMapping("hidden")
+    fun getHidden(): Props {
+        return Props(jwtSecret, pwSalt1, pwSalt2)
+    }
+
+    data class Props(val jwtSecret: String, val pwSalt1: String, val pwSalt2: String)
 }
