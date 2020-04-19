c05e7c598ccf6bce248a01361eb7d273625fe51c
packowitz <packowitz@gmail.com>
2020-04-19 20:24:55 +0200

    jewelry

--- src/main/kotlin/io/pacworx/ambrosia/gear/JewelryController.kt
+++ src/main/kotlin/io/pacworx/ambrosia/gear/JewelryController.kt
@@ -16,19 +16,4 @@ class JewelryController(private val jewelryRepository: JewelryRepository) {
     fun getJewelries(@ModelAttribute("player") player: Player): List<Jewelry> {
         return jewelryRepository.findAllByPlayerId(player.id)
     }
-
-    @PostMapping("merge/{type}/{lvl}")
-    @Transactional
-    fun mergeJewels(@ModelAttribute("player") player: Player, @PathVariable type: JewelType, @PathVariable lvl: Int): PlayerActionResponse {
-        if (lvl == 10) {
-            throw RuntimeException("You cannot merge jewels of level 10 as it is already highest level")
-        }
-        val jewelry = jewelryRepository.findByPlayerIdAndType(player.id, type)
-        if (jewelry == null || jewelry.getAmount(lvl) < 4) {
-            throw RuntimeException("You don't have enough jewels of $type $lvl* to merge them into a higher level")
-        }
-        jewelry.increaseAmount(lvl, -4)
-        jewelry.increaseAmount((lvl + 1), 1)
-        return PlayerActionResponse(jewelries = listOf(jewelry))
-    }
 }
