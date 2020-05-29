e3425cb33c2e02afa7e4d54b27d446bb38abc610
packowitz <packowitz@gmail.com>
2020-05-29 12:17:07 +0200

    added dexterity and resistance buff and debuff

--- src/main/kotlin/io/pacworx/ambrosia/battle/Buff.kt
+++ src/main/kotlin/io/pacworx/ambrosia/battle/Buff.kt
@@ -22,10 +22,12 @@ enum class Buff(
     COUNTERATTACK(BuffType.BUFF, PropertyType.COUNTERATTACK_BUFF, "Increases the possibility to counter an attack"),
     CRIT_BUFF(BuffType.BUFF, PropertyType.CRIT_BUFF, "Increases hero's chance to land a critical hit"),
     CRIT_MULT_BUFF(BuffType.BUFF, PropertyType.CRIT_MULT_BUFF, "Increases hero's damage multiplier for critical hits"),
+    DEXTERITY_BUFF(BuffType.BUFF, PropertyType.DEXTERITY_BUFF, "Increases hero's dexterity"),
     DEATHSHIELD(BuffType.BUFF, PropertyType.DEATHSHIELD_BUFF, "Hero cannot die (stay at 1 HP lowest)"),
     DODGE_BUFF(BuffType.BUFF, PropertyType.DODGE_BUFF, "Increases hero's chance to dodge an attack"),
     HASTE(BuffType.BUFF, PropertyType.HASTE_BUFF, "Increases hero's speed"),
     HEAL_OVER_TIME(BuffType.BUFF, PropertyType.HOT_BUFF, "Healing the hero in the beginning of his turn"),
+    RESISTANCE_BUFF(BuffType.BUFF, PropertyType.RESISTANCE_BUFF, "Increases hero's resistance"),
     SHIELD(BuffType.BUFF, PropertyType.SHIELD_BUFF, "Shield absorbs incoming damage (before armor)"),
     STRENGTH_BUFF(BuffType.BUFF, PropertyType.STRENGTH_BUFF, "Increases hero's strength"),
     TAUNT_BUFF(BuffType.BUFF, PropertyType.TAUNT_BUFF, "Taunting heroes must be attacked first"),
@@ -40,8 +42,10 @@ enum class Buff(
     },
     CRIT_DEBUFF(BuffType.DEBUFF, PropertyType.CRIT_DEBUFF, "Reducing hero's chance to land a critical hit"),
     CRIT_MULT_DEBUFF(BuffType.DEBUFF, PropertyType.CRIT_MULT_DEBUFF, "Reducing hero's damage multiplier for critical hits"),
+    DEXTERITY_DEBUFF(BuffType.DEBUFF, PropertyType.DEXTERITY_DEBUFF, "Reducing hero's dexterity"),
     DAMAGE_OVER_TIME(BuffType.DEBUFF, PropertyType.DOT_DEBUFF, "Dealing damage to the hero in the beginning of his turn"),
     HEAL_BLOCK(BuffType.DEBUFF, PropertyType.HEAL_BLOCK_DEBUFF, "Reducing any healing the hero receives"),
+    RESISTANCE_DEBUFF(BuffType.DEBUFF, PropertyType.RESISTANCE_DEBUFF, "Reducing hero's resistance"),
     SLOW(BuffType.DEBUFF, PropertyType.SLOW_DEBUFF, "Reducing hero's speed"),
     STUN(BuffType.DEBUFF, PropertyType.STUN_DEBUFF, "Stunned heroes are skipping their turn") {
         override fun applyEffect(battle: Battle, hero: BattleHero, buff: BattleHeroBuff, propertyService: PropertyService) {
