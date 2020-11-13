61c8425b56ce7209511cbebdedaafc53b24b9f53
Philipp Schumacher <packowitz@gmail.com>
2020-11-13 15:32:34 +0100

    better error message

--- src/main/kotlin/io/pacworx/ambrosia/exceptions/EntityNotFoundException.kt
+++ src/main/kotlin/io/pacworx/ambrosia/exceptions/EntityNotFoundException.kt
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus
 
 class EntityNotFoundException(val player: Player, val entityName: String, val entityId: Long) : AmbrosiaException, RuntimeException("Requested entity was not found") {
     override val title: String = "Unknown $entityName"
-    override val message: String = "Requested $entityName does not exist. Please refresh your view."
+    override val message: String = "Requested $entityName (#$entityId) does not exist. Please refresh your view."
     override val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST
 }
